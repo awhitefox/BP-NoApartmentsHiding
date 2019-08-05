1e4b45d6a001dda0e37b3eb389b1a82dc2824707
Fox <ignat.k.150@gmail.com>
2019-08-05 22:21:33 +0300

    Initial commit

---
+++ Hooks.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using UnityEngine;
+using UniversalUnityHooks;
+
+namespace NoApartmentsHiding
+{
+    public static class Hooks
+    {
+        private const string ConfigFilePath = "Plugins/NoApartmentsHiding-Settings.json";
+        private static Variables Vars = new Variables();
+
+        [Hook("SvManager.StartServer")]
+        public static void StartServer(SvManager svManager)
+        {
+            Debug.Log("Initializing NoApartmentsHiding...");
+            if (File.Exists(ConfigFilePath))
+            {
+                try { Vars = JsonConvert.DeserializeObject<Variables>(File.ReadAllText(ConfigFilePath)); }
+                catch (Exception) { Debug.Log($"[ERROR] Can't deserialize {ConfigFilePath}. Using default values."); }
+            }
+            else
+            {
+                Debug.Log("Config file for NoApartmentsHiding not found. Creating one...");
+                File.WriteAllText(ConfigFilePath, JsonConvert.SerializeObject(Vars, Formatting.Indented));
+            }
+        }
+
+        [Hook("SvPlayer.SvEnterPlace")]
+        public static bool SvEnterPlace(SvPlayer svPlayer, ref int door, ref ShPlayer shPlayer)
+        {
+            if (shPlayer.wantedLevel > Vars.MaxWantedLevel && svPlayer.entity.manager.FindByID<ShDoor>(door) is ShApartment)
+            {
+                svPlayer.ShowMessageInChat($"<color={Vars.MessageColor}>{Vars.CantEnterMessage}</color>"); // TODO
+                return true;
+            }
+            return false;
+        }
+
+        [Hook("SvPlayer.SvInvite")]
+        public static bool SvInvite(SvPlayer svPlayer, ref int targetId)
+        {
+            var target = svPlayer.entity.manager.FindByID<ShPlayer>(targetId);
+            if (target.wantedLevel > Vars.MaxWantedLevel)
+            {
+                svPlayer.ShowMessageInChat($"<color={Vars.MessageColor}>{Vars.CantInviteMessage}</color>");
+                return true;
+            }
+            return false;
+        }
+
+        private static void ShowMessageInChat(this SvPlayer player, string message) => player.Send(SvSendType.Self, Channel.Unsequenced, ClPacket.GameMessage, message);
+    }
+}
---
+++ Variables.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace NoApartmentsHiding
+{
+    [Serializable]
+    public class Variables
+    {
+        [JsonProperty]
+        public int MaxWantedLevel = 0;
+        [JsonProperty]
+        public string MessageColor = "#e2a90b";
+        [JsonProperty]
+        public string CantEnterMessage = "You can't enter any apartments because of wanted level.";
+        [JsonProperty]
+        public string CantInviteMessage = "You can't invite wanted criminals.";
+    }
+}
