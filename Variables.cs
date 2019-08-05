a27a68aeb173d5fb7c39661658938b9c87d2695f
Fox <ignat.k.150@gmail.com>
2019-08-05 22:25:33 +0300

    Licensing

--- Variables.cs
+++ Variables.cs
@@ -1,4 +1,21 @@
-﻿using Newtonsoft.Json;
+﻿/*
+    This file is part of NoApartmentsHiding.
+
+    NoApartmentsHiding is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    NoApartmentsHiding is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with NoApartmentsHiding. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using Newtonsoft.Json;
 using System;
 
 namespace NoApartmentsHiding
